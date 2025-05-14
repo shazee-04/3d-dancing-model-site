0c5abba5f40099ed9f4f9be13e5d026741ee6374
shazee-04 <mgssrangajeewa@gmail.com>
2025-05-14 13:58:46 +0530

    ✨ add: loader animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
-const bgText_Translate = -200;
+window.addEventListener("load", () => {
+    gsap.to(".loader-container", {
+        opacity: 0,
+        duration: 0.8,
+        delay: 1,
+        ease: "power2.out",
+        onComplete: () => {
+            document.querySelector("loader-container").style.display = "none";
+        }
+    });
 
-gsap.to(".bg-text-1", { x: bgText_Translate, duration: 2, ease: "power3.inOut" });
-gsap.to(".bg-text-2", { x: -bgText_Translate, duration: 2, ease: "power3.inOut" });
+    const bgText_Translate = -200;
+
+    gsap.to(".bg-text-1", { x: bgText_Translate, duration: 2, delay: 1, ease: "power3.inOut" });
+    gsap.to(".bg-text-2", { x: -bgText_Translate, duration: 2, delay: 1, ease: "power3.inOut" });
+});
